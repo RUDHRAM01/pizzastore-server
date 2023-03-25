b3e4543d10833f6940529357f926a7c962ad8667
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-03-25 18:52:46 +0530

    adding Users router

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const dotenv = require('dotenv')
-dotenv.config()
 const PORT = process.env.PORT || 3000
+const Users = require('./router/Users')
 
 app.use(cors());
 app.options('*', cors())
 app.use(bodyParser.json());
-    
+app.use('/users', Users)
+
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT)
 })
