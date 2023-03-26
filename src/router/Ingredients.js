52a5e96830fa033775cf748d3ccc1d96e1873c14
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-03-26 14:35:04 +0530

    auth-middleware update

--- src/router/Ingredients.js
+++ src/router/Ingredients.js
@@ -1,13 +1,10 @@
 const express = require('express')
-
 const router = express.Router()
-
-
 const pool = require('../db/Db')
-
+const auth = require('../middleware/auth')
 
 // Get all ingredients
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
     pool.query('SELECT * FROM ingredients ORDER BY id ASC', (error, results) => {
         if (error) {
             throw error
