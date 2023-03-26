b744ccb843d6ed94de33b49c84f842a8cfd648f7
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-03-26 12:08:09 +0530

    update

--- src/server.js
+++ src/server.js
@@ -6,11 +6,17 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000
 const Users = require('./router/Users')
+const Cart = require('./router/Cart')
+const Ingredients = require('./router/Ingredients')
+
 
 app.use(cors());
 app.options('*', cors())
 app.use(bodyParser.json());
 app.use('/users', Users)
+app.use('/cart', Cart)
+app.use('/ingredients', Ingredients)
+
 
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT)
